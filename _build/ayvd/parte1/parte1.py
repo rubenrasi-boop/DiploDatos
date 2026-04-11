92a41afd20b3d0572588bf82c0ed745054e7e4fb
Rubén Rasi <rubenrasi-boop@users.noreply.github.com>
2026-04-11 20:40:36 -0300

    Actualizar parte1.py: carga de datos y generación de gráfico

--- _build/ayvd/parte1/parte1.py
+++ _build/ayvd/parte1/parte1.py
@@ -10,6 +10,10 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 seaborn.set_context('talk')
 
-df = pd.read_csv('data/sysarmy_survey_2026_processed.csv', keep_default_na=False)
+df = pd.read_csv('data/sysarmy_survey_2026_processed.csv')
 
-print(df[:10])
+#print(df[:10])
+print(df['salary_monthly_NETO'].quantile(0.75) - df['salary_monthly_NETO'].quantile(0.25))
+plt.hist(df['salary_monthly_NETO'], bins=40, density=True, alpha=0.7)
+plt.savefig('grafico.png')                                
+print("✅ Guardado en grafico.png")        
